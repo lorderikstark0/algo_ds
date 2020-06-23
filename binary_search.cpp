41455445c7cf724ccada0a04ea620579cdfe027e
lorderikstark0 <sagnik.chatterjee61@gmail.com>
2020-06-24 00:42:21 +0530

    added partial binary search in cpp

---
+++ binary_search.cpp
@@ -0,0 +1,24 @@
+/*
+Implementing binary search in cpp
+*/
+
+#include <iostream>
+#include <vector>
+using namespace std;
+#define pb push_back
+
+
+int main(){
+    int t;//the number of testcases
+    cin >> t;
+    while(t--){
+        vector<int>arr;
+        int p;//the size of the vector 
+        for(int i=0;i<p;i++){
+            int h;
+            cin >> h;
+            arr.pb(h);
+        }
+        
+    }
+}
